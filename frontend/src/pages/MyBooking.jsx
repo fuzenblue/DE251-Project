ba373a4fdb228aa332772cf21b6f162fadc9f51e
fuzenblue <primrosemprst@gmail.com>
2024-11-29 00:07:33 +0700

    update 29/11

--- frontend/src/pages/MyBooking.jsx
+++ frontend/src/pages/MyBooking.jsx
@@ -17,7 +17,7 @@ const MyBooking = () => {
     <div className="flex-1 p-8 bg-gray-50">
       <h2 className="text-left text-2xl font-bold text-gray-800 mb-6">My Orders</h2>
       <div className="flex flex-col gap-4 m-4">
-          <div className="flex items-center justify-between mx-8 p-4 border border-gray-300 rounded-lg bg-white">
+          <div className="flex items-center justify-between mx-8 gap-8 p-4 border border-gray-300 rounded-lg bg-white">
               <img src={assets.profile_pic} alt="" className="w-12 h-12" />
               {/* Workshop Name */}
               <p className="flex-1 text-sm text-gray-600 mx-4"> workshop1</p>
@@ -29,10 +29,10 @@ const MyBooking = () => {
               <p className="text-gray-700 font-medium">3 x 245.00 = 735.00</p>
               {/* Workshop Status */}
               <p className="text-sm flex items-center">
-                <span className="text-red-500 mr-1">&#x25cf;</span>
+                <span className="text-primary mr-1">&#x25cf;</span>
                 <b>on Booking</b>
               </p>
-              <button className="btn btn-outline btn-error btn-sm">get Ticket</button>
+              <button className="btn btn-outline btn-primary btn-md">Get Ticket</button>
           </div>
       </div>
     </div>
