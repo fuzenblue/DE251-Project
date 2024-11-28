cb54781fc8eb2f0b2741a997fb2f4deca0c64983
fuzenblue <primrosemprst@gmail.com>
2024-11-29 00:03:08 +0700

    update 29/11

--- frontend/src/pages/MyBooking.jsx
+++ frontend/src/pages/MyBooking.jsx
@@ -19,14 +19,20 @@ const MyBooking = () => {
       <div className="flex flex-col gap-4 m-4">
           <div className="flex items-center justify-between mx-8 p-4 border border-gray-300 rounded-lg bg-white">
               <img src={assets.profile_pic} alt="" className="w-12 h-12" />
-              <p className="flex-1 text-sm text-gray-600 mx-4"> workshop1 x 1 people</p>
-              <p className="text-gray-700 font-medium">245.00</p>
-              <p className="text-gray-600">Items: 1</p>
+              {/* Workshop Name */}
+              <p className="flex-1 text-sm text-gray-600 mx-4"> workshop1</p>
+              {/* Workshop price */}
+              <p className="text-gray-700 font-medium">245.00 </p>
+              {/* Number of members */}
+              <p className="text-gray-600">Items: 3</p>
+              {/* Workshop total price */}
+              <p className="text-gray-700 font-medium">3 x 245.00 = 735.00</p>
+              {/* Workshop Status */}
               <p className="text-sm flex items-center">
                 <span className="text-red-500 mr-1">&#x25cf;</span>
                 <b>on Booking</b>
               </p>
-              <button className="btn btn-outline btn-error btn-sm">Track Order</button>
+              <button className="btn btn-outline btn-error btn-sm">get Ticket</button>
           </div>
       </div>
     </div>
