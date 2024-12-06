251b9472f2486509301163273a272264997734ec
fuzenblue <primrosemprst@gmail.com>
2024-12-07 01:47:29 +0700

    update 7/12

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -4,23 +4,23 @@ import bcrypt from "bcrypt"
 import validator from "validator"
 
 // register user
-const registerUser = async ( req,res ) => {
+const registerUser = async (req, res) => {
 
     try {
 
-        const { name, email, password  } = req.body
+        const { name, email, password } = req.body
 
-        if ( !name || !password || !email) {
+        if (!name || !password || !email) {
             return res.json({ success: false, message: "Missing Details" })
         }
 
         // validating email format & strong password
-        if (!validator.isEmail( email )) {
-            return res.json({ success:false, message:"Enter a valid email" })
+        if (!validator.isEmail(email)) {
+            return res.json({ success: false, message: "Enter a valid email" })
         }
 
-        if (password.length <8) {
-            return res.json({ success:false, message:"Enter a strong password" })
+        if (password.length < 8) {
+            return res.json({ success: false, message: "Enter a strong password" })
         }
 
         // hashing user password
@@ -30,47 +30,85 @@ const registerUser = async ( req,res ) => {
         const userData = {
             name,
             email,
-            password : hashPassword
+            password: hashPassword
         }
-        
+
         const newUser = new userModel(userData)
         const user = await newUser.save()
 
-        const token = jwt.sign({id:user._id}, process.env.JWT_SECRET)
-        res.json({success:true, token})
-        
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+        res.json({ success: true, token })
+
     } catch (error) {
         console.log(error)
-        res.json({success:false, message:error.message})
+        res.json({ success: false, message: error.message })
     }
 }
 
 // login user
-const loginUser = async (req,res) => {
+const loginUser = async (req, res) => {
 
     try {
         const { email, password } = req.body
         const user = await userModel.findOne({ email })
 
         if (!user) {
-            return res.json({success:false, message:"User Doesn't exist"})
+            return res.json({ success: false, message: "User Doesn't exist" })
         }
 
         const isMatch = await bcrypt.compare(password, user.password)
 
         if (isMatch) {
-            const token = jwt.sign({id:user._id}, process.env.JWT_SECRET)
-            res.json({success:true, token})
+            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+            res.json({ success: true, token })
 
         } else {
-            return res.json({success:false, message:"Invalid credentials"})
+            return res.json({ success: false, message: "Invalid credentials" })
         }
 
     } catch (error) {
         console.log(error)
-        res.json({success:false, message:error.message})
+        res.json({ success: false, message: error.message })
+    }
+}
+
+// API to get user profile
+const getProfile = async (req, res) => {
+
+    try {
+        const userId = req.body.userId
+        // console.log(userId)
+
+        const userData = await userModel.findById(userId).select('-password')
+        return res.json({ success: true, userData })
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
+
+const updateProfile = async (req, res) => {
+    try {
+
+        const { userId, name, first_name, last_name, phone, dob, gender } = req.body
+        const imageFile = req.file
+
+        if (!name || !first_name || !last_name || !phone || !dob || !gender) {
+            return res.json({ success: false, message: "Data Missing" })
+        }
+
+        await userData.findByIdAndUpdate(userId, { name, first_name, last_name, phone, address: JSON.parse(address), dob, gender })
+
+        if (imageFile) {
+            // 9.39
+        }
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
     }
 }
 
 
-export { registerUser, loginUser }
+export { registerUser, loginUser, getProfile, updateProfile }
---
+++ backend/middlewares/authUser.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken'
+
+// User authentication middleware
+const authUser = async (req, res, next) => {
+    try {
+
+        const { token } = req.headers
+        if (!token) {
+            return res.json({ success: false, message: 'Not Authorized, Login Again' })
+        }
+
+        const token_decode = jwt.verify(token, process.env.JWT_SECRET)
+        req.body.userId = token_decode.id
+        // console.log("Decoded Token:", token_decode)
+        
+        next()
+    } catch (error) {
+        console.error(error);
+        res.json({ success: false, message:error.message})
+    }
+}
+
+export default authUser
