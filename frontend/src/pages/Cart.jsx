203d9887fd5343dc51b40ab57df0d359d2853395
Piyafame16 <piyathida.mah@g.swu.ac.th>
2024-12-18 14:23:43 +0700

    update 19/12

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,27 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const Cart = () => {
-  return (
-    <div>
-      My Cart
-
-      <div>
-        Product
-      </div>
-
-      <div>
-        Workshop
-      </div>
-
-      <div>
-        Summary
-
-
-        <button>Check Out</button>
-      </div>
-
-    </div>
-  )
-}
-
-export default Cart
+const Cart = () => 
+  
+export default Cart;
